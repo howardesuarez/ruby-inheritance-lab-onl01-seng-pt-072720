05ecac7f9adbec1923c456a63578c99609f393cb
howard suarez <howardesuarez@gmail.com>
2020-09-09 04:56:08 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,9 +8,10 @@ class Teacher < User
                "programming computers hacking learning terminal",
                "bash Ruby rvm update certs"]
 
-
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]	  def teach
+ def teach
 
     KNOWLEDGE.sample
 
-  e
+end
+
+end
